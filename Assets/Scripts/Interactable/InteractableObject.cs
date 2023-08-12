431b5df4c39617ae8893177ffda30feb6ae71875
radke <radkewolf@web.de>
2023-08-12 14:15:51 +0200

    reduce distance slightly

--- Assets/Scripts/Interactable/InteractableObject.cs
+++ Assets/Scripts/Interactable/InteractableObject.cs
@@ -11,7 +11,7 @@ public abstract class InteractableObject : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         var layer_mask = LayerMask.GetMask("PickUp");
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 20f, layer_mask))
+        if (Physics.Raycast(ray, out hit, 19f, layer_mask))
         {
             var interactable = hit.collider.gameObject.GetComponent<InteractableObject>();
             return interactable;
