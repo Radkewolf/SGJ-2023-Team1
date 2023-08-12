7139106fb72babcaca2475083f65aecd907a3aa3
radke <radkewolf@web.de>
2023-08-12 07:34:01 +0200

    made torch interactable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
 
     private bool CanJump;
 
+    #region MonoBehaviour
     void Start()
     {
         _Rigidbody = GetComponent<Rigidbody>();
@@ -28,43 +30,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * ForwardMultiplayer * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= transform.forward * BackwardMultiplayer * Time.deltaTime;
-        }
-
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.right * StrafeMultiplier * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.position -= transform.right * StrafeMultiplier * Time.deltaTime;
-        }
-
-        if(Input.GetKeyDown(KeyCode.Space) && CanJump)
-        {
-            _Rigidbody.AddForce(Vector3.up * JumpForceMultiplier);
-        }
-
-        var xDelta = Input.GetAxis("Mouse X");
-        if (xDelta != 0)
-        {
-            transform.Rotate(0, xDelta * Time.deltaTime * LookMultiplier, 0);
-        }
-
-        var yDelta = Input.GetAxis("Mouse Y");
-        if (yDelta != 0)
-        {
-            GameMaster.Camera.transform.RotateAround(transform.position, Vector3.right, yDelta * LookMultiplier * Time.deltaTime);
-
-            Vector3 dir = (GameMaster.Camera.transform.position - transform.position).normalized;
-            GameMaster.Camera.transform.position = transform.position + dir * 10;
-        }
+        Movement();
+        Interact();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -78,5 +45,62 @@ public class PlayerController : MonoBehaviour
     void OnCollisionExit(Collision collision)
     {
         CanJump = false;
+    }
+    #endregion
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * ForwardMultiplayer * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * BackwardMultiplayer * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * StrafeMultiplier * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * StrafeMultiplier * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump)
+        {
+            _Rigidbody.AddForce(Vector3.up * JumpForceMultiplier);
+        }
+
+        var xDelta = Input.GetAxis("Mouse X");
+        if (xDelta != 0)
+        {
+            transform.Rotate(0, xDelta * Time.deltaTime * LookMultiplier, 0);
+        }
+
+        var yDelta = Input.GetAxis("Mouse Y");
+        if (yDelta != 0)
+        {
+            GameMaster.Camera.transform.RotateAround(transform.position + (Vector3.up * 2), Vector3.right, yDelta * LookMultiplier * Time.deltaTime);
+
+            Vector3 dir = (GameMaster.Camera.transform.position - transform.position).normalized;
+            GameMaster.Camera.transform.position = transform.position + dir * 5;
+        }
+    }
+
+    private void Interact()
+    {
+        if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var interactable = hit.collider.gameObject.GetComponent<IInteractableObject>();
+                if (interactable != null)
+                    interactable.Interact();
+            }
+        }
     }
 }
